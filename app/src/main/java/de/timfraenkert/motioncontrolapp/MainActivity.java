adc92b3d600514c2820729f64e1d5baeee7b113c
tim fraenkert <tim@fraenkert.de>
2021-11-10 11:47:58 +0100

    Motion + Rotation

--- app/src/main/java/de/timfraenkert/motioncontrolapp/MainActivity.java
+++ app/src/main/java/de/timfraenkert/motioncontrolapp/MainActivity.java
@@ -7,38 +7,98 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.hardware.TriggerEvent;
-import android.hardware.TriggerEventListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView txt_acceleration ;
-    TextView txt_currentAccel ;
-    TextView txt_prevAccel ;
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView txt_accelerationX ;
+    TextView txt_accelerationY ;
+    TextView txt_accelerationZ ;
+
+    TextView txt_rotationX ;
+    TextView txt_rotationY ;
+    TextView txt_rotationZ ;
+
+
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     private double acceerationCurrentValue;
     private double acceerationPreviousValue = 0;
 
+    float[] mGravity = new float[3];
+    float[] mGeomagnetic = new float[3];
+
     private SensorManager sensorManager;
     private Sensor sensor;
+    private Sensor linearSensor;
+    private Sensor geomagneticSensor;
 
 
-    private SensorEventListener sensorEventListener = new SensorEventListener() {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        handleRotation(event);
+        handleAccelaration(event);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void handleAccelaration(SensorEvent event){
+        if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+            float[] accel = new float[3];
+            accel = event.values;
+            float x = accel[0];
+            float y = accel[1];
+            float z = accel[2];
+
+            txt_accelerationX.setText("X: "+ x + "m/S^2");
+            txt_accelerationY.setText("Y: "+ y + "m/S^2");
+            txt_accelerationZ.setText("Z: "+ z + "m/S^2");
+        }
+
+    }
+
+    private void handleRotation(SensorEvent event){
+        // norden, vertikal, horizontal
+        float azimuth,pitch, roll;
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                azimuth = orientation[0]; // orientation contains: azimuth, pitch and roll
+                pitch = orientation[1];
+                roll = orientation[2];
+
+                txt_rotationX.setText("Azimuth : " +  Math.toDegrees(azimuth));
+                txt_rotationY.setText("Pitch : " + (Math.toDegrees(pitch) + 90.0f));
+                txt_rotationZ.setText("roll : " + Math.toDegrees(roll));
+            }
+        }
+
+    }
+
+
+    private SensorEventListener rotationListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
 
-            acceerationCurrentValue = Math.sqrt( (x * x + y * y + z * z));
-            double changeInAccelleration = Math.abs(acceerationCurrentValue - acceerationPreviousValue);
-            acceerationPreviousValue = acceerationCurrentValue;
-
-            // update Text views
-            txt_currentAccel.setText("Current = " + acceerationCurrentValue);
-            txt_prevAccel.setText("Prev = " + acceerationPreviousValue);
-            txt_acceleration.setText("Acceleation change =" + changeInAccelleration);
         }
 
         @Override
@@ -46,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
-
 
 
     @Override
@@ -57,23 +116,35 @@ public class MainActivity extends AppCompatActivity {
         // Linear ACCELERATION sensor
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        linearSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        geomagneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-        txt_acceleration = (TextView) findViewById(R.id.acceleration);
-        txt_currentAccel = (TextView)  findViewById(R.id.txt_currentAccel);
-        txt_prevAccel = (TextView)  findViewById(R.id.txt_prevAccel);
+        txt_accelerationX = (TextView) findViewById(R.id.txt_accelerationX);
+        txt_accelerationY = (TextView) findViewById(R.id.txt_accelerationY);
+        txt_accelerationZ = (TextView) findViewById(R.id.txt_accelerationZ);
+
+        txt_rotationX = (TextView) findViewById(R.id.txt_xRotation);
+        txt_rotationY = (TextView) findViewById(R.id.txt_yRotation);
+        txt_rotationZ = (TextView) findViewById(R.id.txt_zRotation);
 
     }
 
     public void onStart() {
         super.onStart();
         // enable our sensor when the activity is resumed, ask for
+        sensorManager.registerListener(this, sensor, 100000);
+        sensorManager.registerListener(this, linearSensor, 100000);
+        sensorManager.registerListener(this, geomagneticSensor, 100000);
         // 10 ms updates.
-        sensorManager.registerListener(sensorEventListener, sensor, 10000);
+
+
     }
     public void onStop() {
         super.onStop();
         // make sure to turn our sensor off when the activity is paused
-        sensorManager.unregisterListener(sensorEventListener);
+        sensorManager.unregisterListener(this, sensor);
+        sensorManager.unregisterListener(this, linearSensor);
+        sensorManager.unregisterListener(this, geomagneticSensor);
     }
 
 
